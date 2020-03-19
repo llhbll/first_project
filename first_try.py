b5bd840cbfdd23bcf04b375efb6337d49617dfb3
llhbll <llhbll00@gmail.com>
2020-03-20 06:28:49 +0900

    github가 무엇인지 몰라 이것 저것 해 보는중 pc home에서 commit push 해보고 pc work에서 어떻게 보여지는지 확인해보려함

--- first_try.py
+++ first_try.py
@@ -1,15 +1,41 @@
-import  sys
 
-option = sys.argv[1]
 
-if option == '-a':
-    memo = sys.argv[2]
-    f = open('memo.txt', 'a')
-    f.write(memo)
-    f.write('\n')
-    f.close()
-elif option == '-v':
-    f = open('memo.txt', 'r')
-    memo = f.read()
-    f.close()
-    print(memo)
+
+# 파일 읽고 쓰기
+# import  sys
+#
+# option = sys.argv[1]
+#
+# if option == '-a':
+#     memo = sys.argv[2]
+#     f = open('memo.txt', 'a')
+#     f.write(memo)
+#     f.write('\n')
+#     f.close()
+# elif option == '-v':
+#     f = open('memo.txt', 'r')
+#     memo = f.read()
+#     f.close()
+#     print(memo)
+
+# pandas Seris
+# from pandas import Series
+#
+# data = [100, 200, 300, 400]
+# date = ['2018-08-01', '2019-08-01', '2020-08-01', '2020-08-02']
+# s = Series(data, index = date)
+# print(s[[0, 2]])
+
+# pandas dataframe
+# from pandas import DataFrame
+#
+# data = {'open':[100, 200], 'high':[110, 210]}
+# df = DataFrame(data, index=['2018-01-01', '2018-01-02'])
+#
+# print(df)
+
+import pandas as pd
+
+df = pd.read_excel("test.xlsx")
+df = df.set_index('date')
+print(df)
